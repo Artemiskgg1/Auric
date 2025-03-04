e5b84e52800a36ad58afe112e7f989fbb388948a
iynes <abhaghildiyal@gmail.com>
2025-03-04 22:42:35 +0530

    Remove Map and DottedMap components; update Hero component to use ImagesSlider for enhanced visual presentation.

--- src/app/dashboard/[Id]/(home)/page.tsx
+++ src/app/dashboard/[Id]/(home)/page.tsx
@@ -1,12 +1,9 @@
-import Map from "@/components/Map";
 import React from "react";
 
 const page = () => {
   return (
     <div>
-      <div>
-        <Map />
-      </div>
+      <div></div>
     </div>
   );
 };
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,25 +1,13 @@
 "use client";
 import React from "react";
 import Nav from "./Nav";
-import DottedMap from "./DottedMap";
+import { ImagesSliderDemo } from "./BgImageSlider";
 
 export default function Hero() {
   return (
     <div className="w-full pt-16 relative">
-      <div className="relative m-8 md:m-20 z-10 flex flex-col items-center justify-center text-center text-gray-900 h-full px-4">
-        <h1 className="text-3xl md:text-7xl font-bold mb-4 font-quichesans">
-          Visualize the disasters in{" "}
-          <span className="bg-gradient-to-r from-blue-600 via-blue-400 to-blue-500 text-transparent bg-clip-text">
-            Real Time
-          </span>
-        </h1>
-
-        <p className="max-w-2xl text-sm md:text-lg mb-2 text-gray-700">
-          {
-            "Empowering Disaster Response with AI-Powered Insights. Stay ahead of emergencies with real-time predictions, risk analysis, and intelligent response strategies."
-          }
-        </p>
-        <DottedMap />
+      <div className="relative z-10 flex flex-col items-center justify-center text-center text-gray-900 h-full ">
+        <ImagesSliderDemo />
       </div>
     </div>
   );
